b2f38b096e7f6d078df409ada3beaa0e471b7b25
ssteiger <soren1@gmx.net>
2019-06-01 20:50:58 +0200

    Wallet: Add tests for sending/receiving erc20 and erc721 tokens

--- wallet/test/wallet.js
+++ wallet/test/wallet.js
@@ -1,5 +1,95 @@
-const wallet = artifacts.require('wallet');
+const erc20  = artifacts.require('erc20');
+const erc721 = artifacts.require('erc721');
+const erc777 = artifacts.require('erc777');
+
+const walletAbstraction = artifacts.require('wallet');
+
+let wallet;
 
 contract('wallet', accounts => {
+  beforeEach(async () => {
+    wallet = await walletAbstraction.new({ from: accounts[0] });
+  });
 
+  it('...should accept erc20 token deposit.', async () => {
+    const sender = accounts[0];
+
+    const erc20Token = await erc20.deployed();
+
+    let balance = await erc20Token.balanceOf.call(wallet.address);
+    const wallet_starting_balance = balance.toString();
+
+    const amount = 100;
+
+    await erc20Token.transfer(wallet.address, amount, { from: sender });
+
+    balance = await erc20Token.balanceOf.call(wallet.address);
+    const wallet_ending_balance = balance.toString();
+
+    assert.equal(
+      wallet_starting_balance,
+      wallet_ending_balance - amount,
+      'ERC20 tokens were not correctly deposited'
+    );
+  });
+
+  it('...should send erc20 tokens.', async () => {
+    const walletOwner = accounts[0];
+    const receiver = accounts[1];
+
+    const amount = 100;
+
+    const erc20Token = await erc20.deployed();
+    await erc20Token.transfer(wallet.address, amount, { from: accounts[0] });
+
+    //let balance = await erc20Token.balanceOf.call(wallet.address);
+    //const wallet_starting_balance = balance.toString();
+
+    let balance = await erc20Token.balanceOf.call(receiver);
+    const receiver_starting_balance = balance.toString();
+
+    await wallet.sendERC20(erc20Token.address, receiver, amount, { from: walletOwner, gas: 4000000 });
+
+    //balance = await erc20Token.balanceOf.call(wallet.address);
+    //const wallet_ending_balance = balance.toString();
+
+    balance = await erc20Token.balanceOf.call(receiver);
+    const receiver_ending_balance = balance.toString();
+
+    assert.equal(
+      receiver_starting_balance,
+      receiver_ending_balance - amount,
+      'ERC20 tokens were not correctly credited'
+    );
+  });
+
+  it('...should accept erc721 token deposit.', async () => {
+    const sender = accounts[0];
+
+    const erc721Token = await erc721.deployed();
+
+    const mintReturnData = await erc721Token.mint();
+    const tokenId = mintReturnData.logs[0].args['1'].toString();
+
+    let balance = await erc721Token.balanceOf.call(wallet.address);
+    const wallet_starting_balance = balance.toString();
+
+    await erc721Token.transferFrom(sender, wallet.address, tokenId, { from: sender });
+
+    balance = await erc721Token.balanceOf.call(wallet.address);
+    const wallet_ending_balance = balance.toString();
+
+    const ownerOfNFT = await erc721Token.ownerOf.call(tokenId);
+
+    assert.equal(
+      wallet_starting_balance,
+      wallet_ending_balance - 1,
+      'ERC721 token was not correctly deposited'
+    );
+    assert.equal(
+      wallet.address,
+      ownerOfNFT,
+      'ERC721 token was not correctly deposited'
+    );
+  });
 });
