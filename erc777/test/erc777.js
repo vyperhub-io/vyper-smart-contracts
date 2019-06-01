46a4fe43eaba111ea3f1acfc22b6a8bdc9e00d4b
ssteiger <soren1@gmx.net>
2019-06-01 11:45:13 +0200

    ERC777: Add comment about tests

--- erc777/test/erc777.js
+++ erc777/test/erc777.js
@@ -1,5 +1,8 @@
 const erc777 = artifacts.require("erc777");
 
+// Tests for this implementation can be found here:
+// https://github.com/0xjac/ERC777/tree/master/test
+
 const args = {
   name: "MyToken",
   symbol: "MT",
