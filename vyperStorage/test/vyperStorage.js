8768116480e2e716d133692c5610e1016ece8524
ssteiger <soren1@gmx.net>
2019-06-13 22:22:41 +0200

    VyperStorage: Update tests

--- vyperStorage/test/vyperStorage.js
+++ vyperStorage/test/vyperStorage.js
@@ -1,15 +1,29 @@
-const VyperStorage = artifacts.require("VyperStorage");
+const vyperStorage = artifacts.require('vyperStorage');
 
-contract("VyperStorage", () => {
-  it("...should store the value 89.", async () => {
-    const storage = await VyperStorage.deployed();
+contract('vyperStorage', () => {
+  it('...should initialize storage with 0.', async () => {
+    const storage = await vyperStorage.deployed();
 
-    // Set value of 89
+    const storedData = await storage.stored_data();
+
+    assert.equal(
+      storedData,
+      0,
+      'The value 89 was not stored.'
+    );
+  });
+
+  it('...should store the value 89.', async () => {
+    const storage = await vyperStorage.deployed();
+
     await storage.set(89);
 
-    // Get stored value
-    const storedData = await storage.get();
+    const storedData = await storage.stored_data();
 
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+    assert.equal(
+      storedData,
+      89,
+      'The value 89 was not stored.'
+    );
   });
 });
